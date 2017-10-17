65acd86052d177bf38f826da0eef152131116714
Andrew Low <andrew.low@inspection.gc.ca>
2017-10-17 15:51:30 -0400

    Added bbduk_bait, bbduk_filter, dedupe, seal.

--- README.md
+++ README.md
@@ -4,8 +4,26 @@
 
 - BBtools >= 37.23
 
-#### Package currently under development.
+### Package currently under development.
 Installation: 
 - Clone this repository.
 - Move into this directory.
 - `pip3 install -e .`
+
+### Functionality
+##### bbtools
+For all bbtools, additional options can be specified with argument='parameter' passed in after mandatory arguments.
+`bbtools.bbmap(reference, forward_reads, out_file)` Mandatory args: reference file (should be fasta 
+formatted), forward reads (reverse reads will be found automatically if R1/R2 naming convention is used), output file (sam or bam).
+If reverse reads do not follow naming convention, specify with reverse_in='path/to/reads'.
+`bbtools.bbduk_trim(forward_in, forward_out)` Does quality trimming of reads.
+As with bbmap, if R1/R2 naming convention is used, reverse input/output don't need to be specified.
+If convenction isn't followed, specify with reverse_in='path' and reverse_out='path'
+`bbtools.bbduk_bait(reference, forward_in, forward_out)` Baits reads in input that match reference, writes to output.
+As with bbmap, if R1/R2 naming convention is used, reverse input/output don't need to be specified.
+If convenction isn't followed, specify with reverse_in='path' and reverse_out='path'
+`bbtools.bbduk_filter(reference, forward_in, forward_out)` Filters reads in input that match reference, writes clean reads to output.
+As with bbmap, if R1/R2 naming convention is used, reverse input/output don't need to be specified.
+If convenction isn't followed, specify with reverse_in='path' and reverse_out='path'
+`bbtools.dedupe(input, output)` Runs dedupe on input, writes to output.
+`bbtools.seal(reference, forward_in, rpkm)` Runs seal to generate rpkm stats on reference.
--- biotools/bbtools.py
+++ biotools/bbtools.py
@@ -47,7 +47,7 @@ def bbduk_trim(forward_in, forward_out, reverse_in='NA', reverse_out='NA', **kwa
     :param reverse_in: Reverse input reads. Don't need to be specified if R1/R2 naming convention is used.
     :param reverse_out: Reverse output reads. Don't need to be specified if R1/R2 convention is used.
     :param kwargs: Other arguments to give to bbduk in parameter=argument format. See bbduk documentation for full list.
-    :return: out and err: stdout string and stderr string from running bbmap.
+    :return: out and err: stdout string and stderr string from running bbduk.
     """
     options = kwargs_to_string(kwargs)
     cmd = 'which bbduk.sh'
@@ -83,3 +83,106 @@ def bbduk_trim(forward_in, forward_out, reverse_in='NA', reverse_out='NA', **kwa
     out, err = accessoryfunctions.run_subprocess(cmd)
     return out, err
 
+
+def bbduk_bait(ref, forward_in, forward_out, reverse_in='NA', reverse_out='NA', **kwargs):
+    """
+    Uses bbduk to bait out reads that have kmers matching to a reference.
+    :param ref: Reference you want to pull reads out for. Should be in fasta format.
+    :param forward_in: Forward reads you want to quality trim.
+    :param forward_out: Output forward reads.
+    :param reverse_in: Reverse input reads. Don't need to be specified if R1/R2 naming convention is used.
+    :param reverse_out: Reverse output reads. Don't need to be specified if R1/R2 convention is used.
+    :param kwargs: Other arguments to give to bbduk in parameter=argument format. See bbduk documentation for full list.
+    :return: out and err: stdout string and stderr string from running bbduk.
+    """
+    options = kwargs_to_string(kwargs)
+    if os.path.isfile(forward_in.replace('R1', 'R2')) and reverse_in == 'NA':
+        reverse_in = forward_in.replace('R1', 'R2')
+        if reverse_out == 'NA':
+            if 'R1' in forward_out:
+                reverse_out = forward_out.replace('R1', 'R2')
+            else:
+                raise ValueError('If you do not specify reverse_out, forward_out must contain R1.\n\n')
+        cmd = 'bbduk.sh in={} in2={} outm={} outm2={} ref={}{}'.format(forward_in, reverse_in,
+                                                                       forward_out, reverse_out,
+                                                                       ref, options)
+    elif reverse_in == 'NA':
+        cmd = 'bbduk.sh in={} outm={} ref={}{}'.format(forward_in, forward_out, ref, options)
+    else:
+        if reverse_out == 'NA':
+            raise ValueError('Reverse output reads must be specified.')
+        cmd = 'bbduk.sh in={} in2={} outm={} outm2={} ref={}{}'.format(forward_in, reverse_in,
+                                                                       forward_out, reverse_out,
+                                                                       ref, options)
+    out, err = accessoryfunctions.run_subprocess(cmd)
+    return out, err
+
+
+def bbduk_filter(ref, forward_in, forward_out, reverse_in='NA', reverse_out='NA', **kwargs):
+    """
+    Uses bbduk to filter out reads that have kmers matching to a reference.
+    :param ref: Reference you want to pull reads out for. Should be in fasta format.
+    :param forward_in: Forward reads you want to quality trim.
+    :param forward_out: Output forward reads.
+    :param reverse_in: Reverse input reads. Don't need to be specified if R1/R2 naming convention is used.
+    :param reverse_out: Reverse output reads. Don't need to be specified if R1/R2 convention is used.
+    :param kwargs: Other arguments to give to bbduk in parameter=argument format. See bbduk documentation for full list.
+    :return: out and err: stdout string and stderr string from running bbduk.
+    """
+    options = kwargs_to_string(kwargs)
+    if os.path.isfile(forward_in.replace('R1', 'R2')) and reverse_in == 'NA':
+        reverse_in = forward_in.replace('R1', 'R2')
+        if reverse_out == 'NA':
+            if 'R1' in forward_out:
+                reverse_out = forward_out.replace('R1', 'R2')
+            else:
+                raise ValueError('If you do not specify reverse_out, forward_out must contain R1.\n\n')
+        cmd = 'bbduk.sh in={} in2={} out={} out2={} ref={}{}'.format(forward_in, reverse_in,
+                                                                     forward_out, reverse_out,
+                                                                     ref, options)
+    elif reverse_in == 'NA':
+        cmd = 'bbduk.sh in={} out={} ref={}{}'.format(forward_in, forward_out, ref, options)
+    else:
+        if reverse_out == 'NA':
+            raise ValueError('Reverse output reads must be specified.')
+        cmd = 'bbduk.sh in={} in2={} out={} out2={} ref={}{}'.format(forward_in, reverse_in,
+                                                                     forward_out, reverse_out,
+                                                                     ref, options)
+    out, err = accessoryfunctions.run_subprocess(cmd)
+    return out, err
+
+
+def dedupe(infile, outfile, **kwargs):
+    """
+    Runs dedupe from the bbtools package.
+    :param infile: Input file.
+    :param outfile: Output file.
+    :param kwargs: Arguments to give to dedupe in parameter=argument format. See dedupe documentation for full list.
+    :return: out and err: stdout string and stderr string from running dedupe.
+    """
+    options = kwargs_to_string(kwargs)
+    cmd = 'dedupe.sh in={} out={}{}'.format(infile, outfile, options)
+    out, err = accessoryfunctions.run_subprocess(cmd)
+    return out, err
+
+
+def seal(ref, forward_in, output_file, reverse_in='NA', **kwargs):
+    """
+    Runs seal from the bbtools package.
+    :param ref: Reference file, in fasta format.
+    :param forward_in: Forward reads, fastq format.
+    :param output_file: Output file to put rpkm statistics into.
+    :param reverse_in: Reverse reads. Not necessary to specify if in same folder and follow R1/R2 convention.
+    :param kwargs: Arguments to give to seal in parameter=argument format. See seal documentation for full list.
+    :return: out and err: stdout string and stderr string from running dedupe.
+    """
+    options = kwargs_to_string(kwargs)
+    if os.path.isfile(forward_in.replace('R1', 'R2')) and reverse_in == 'NA':
+        reverse_in = forward_in.replace('R1', 'R2')
+        cmd = 'seal.sh ref={} in={} in2={} rpkm={} nodisk{}'.format(ref, forward_in, reverse_in, output_file, options)
+    elif reverse_in == 'NA':
+        cmd = 'seal.sh ref={} in={} rpkm={} nodisk{}'.format(ref, forward_in, output_file, options)
+    else:
+        cmd = 'seal.sh ref={} in={} in2={} rpkm={} nodisk{}'.format(ref, forward_in, reverse_in, output_file, options)
+    out, err = accessoryfunctions.run_subprocess(cmd)
+    return out, err
